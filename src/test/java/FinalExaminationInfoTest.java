5bd62883d3a093438aae7e36b37f2b8da7e66699
johan97lund <johan.lund97@gmail.com>
2023-05-16 23:27:03 +0200

    Test 1 is completed

--- src/test/java/FinalExaminationInfoTest.java
+++ src/test/java/FinalExaminationInfoTest.java
@@ -2,22 +2,19 @@ import com.codeborne.selenide.*;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import org.junit.jupiter.api.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
-import static com.codeborne.selenide.Configuration.*;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FinalExaminationInfoTest implements TestSetup {
 
-
-
     @Test
-    public void testFinalExaminationInformation() throws InterruptedException {
-        Thread.sleep(200);
+    public void testFinalExaminationInformation() throws InterruptedException, IOException {
+
+        Thread.sleep(2000);
         // Click on the menu that is called "Tentamen".
         Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/a").shouldHave(Condition.text("Tentamen")).click();
         Thread.sleep(2000);
@@ -25,39 +22,56 @@ public class FinalExaminationInfoTest implements TestSetup {
         Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/ul/li[2]/a").shouldHave(Condition.text("Tentamensschema")).click();
         Selenide.switchTo().window(1);
         Thread.sleep(1000);
+        // Click on the Log in button in the top right corner.
         Selenide.$("#topnav > a.signin").click();
-        Thread.sleep(2000000000);
+        Thread.sleep(2000);
+        loginKronoxWebsite();
+        Thread.sleep(2000);
 
-        loginWithKronoxCredentials();
+        Selenide.$x("//*[@id=\"login_button\"]").click();
+        Thread.sleep(2000);
+        Selenide.$("body > div.main > div.header > div > div > ul > li:nth-child(9) > a > em > b").click();
+        Thread.sleep(2000);
+
+        String expectedText = "Datum: 2023-05-30";
+        String actualText = Selenide.$("body").getText();
+        assertTrue(actualText.contains(expectedText));
+
+        Thread.sleep(10000);
 
 
     }
 
+    private void loginKronoxWebsite() throws InterruptedException {
+        String passwordKronox = null;
+        String usernameKronox = null;
 
-    // Verify that the web page has the correct information
-    //$("").shouldHave(Condition.text("Aktuellt"));
-    // Add more assertions or verifications as needed
+        try {
+            logger.info("Reading username and password from kronoxConfig.json");
+            // Read the email and password from kronoxConfig.json
+            Gson gson = new Gson();
+            JsonElement config = gson.fromJson(new FileReader("kronoxConfig.json"), JsonElement.class);
+            usernameKronox = config.getAsJsonObject().get("usernameKronox").getAsString();
+            passwordKronox = config.getAsJsonObject().get("passwordKronox").getAsString();
+            logger.info("Read successfully!");
+        } catch (IOException e) {
+            // Handle any exceptions that might occur while reading the file
+            logger.error("Could not read kronoxConfig.json", e);
+            System.exit(1);
+        }
 
+        fillInKronoxUsernameAndPassword(usernameKronox, passwordKronox);
 
-    private void loginWithKronoxCredentials() throws InterruptedException {
-        String kronoxUsername = "kronoxUsername";
-        String kronoxPassword = "kronoxPassword";
-
-        loginToWebsite(kronoxUsername, kronoxPassword);
     }
 
-    private void loginToWebsite(String kronoxUsername, String kronoxPassword) throws InterruptedException {
-        SelenideElement userID = $("#username");
-        userID.setValue(kronoxUsername);
+    private void fillInKronoxUsernameAndPassword(String usernameKronox, String passwordKronox) throws InterruptedException {
+        SelenideElement passID = $("#login_password");
+        passID.setValue(passwordKronox);
         Thread.sleep(1000);
 
-        SelenideElement passID = $("#password");
-        passID.setValue(kronoxPassword);
+        SelenideElement userID = $("#login_username");
+        userID.setValue(usernameKronox);
         Thread.sleep(1000);
-    }
 
-    // Helper method to read credentials from file
-    // private String readCredentialsFromFile(String filePath) throws IOException {
-    //     return new String(Files.readAllBytes(Paths.get(filePath)));
-    // }
+    }
 }
