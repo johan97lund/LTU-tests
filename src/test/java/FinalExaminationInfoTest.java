940102e9aa16d1bca69ac6aad5f4619705f65cef
johan97lund <johan.lund97@gmail.com>
2023-05-17 12:43:33 +0200

    Test 2 is completed

--- src/test/java/FinalExaminationInfoTest.java
+++ src/test/java/FinalExaminationInfoTest.java
@@ -2,44 +2,91 @@ import com.codeborne.selenide.*;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import org.junit.jupiter.api.*;
-
 import java.io.FileReader;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FinalExaminationInfoTest implements TestSetup {
 
+    private static final Logger logger = LoggerFactory.getLogger(FinalExaminationInfoTest.class);
+
+
     @Test
     public void testFinalExaminationInformation() throws InterruptedException, IOException {
+        try {
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
 
-        Thread.sleep(2000);
-        // Click on the menu that is called "Tentamen".
-        Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/a").shouldHave(Condition.text("Tentamen")).click();
-        Thread.sleep(2000);
-        // Click on the menu that is called "Tentamensschema" and a new window pop up for Kronox.
-        Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/ul/li[2]/a").shouldHave(Condition.text("Tentamensschema")).click();
-        Selenide.switchTo().window(1);
-        Thread.sleep(1000);
-        // Click on the Log in button in the top right corner.
-        Selenide.$("#topnav > a.signin").click();
-        Thread.sleep(2000);
-        loginKronoxWebsite();
-        Thread.sleep(2000);
+            // Click on the menu that is called "Tentamen".
+            Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/a")
+                    .shouldHave(Condition.text("Tentamen")).click();
 
-        Selenide.$x("//*[@id=\"login_button\"]").click();
-        Thread.sleep(2000);
-        Selenide.$("body > div.main > div.header > div > div > ul > li:nth-child(9) > a > em > b").click();
-        Thread.sleep(2000);
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
 
-        String expectedText = "Datum: 2023-05-30";
-        String actualText = Selenide.$("body").getText();
-        assertTrue(actualText.contains(expectedText));
+            // Click on the menu that is called "Tentamensschema" and a new window pops up for Kronox.
+            Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[3]/ul/li[2]/a")
+                    .shouldHave(Condition.text("Tentamensschema")).click();
 
-        Thread.sleep(10000);
+            // Switch to the new window for Kronox
+            Selenide.switchTo().window(1);
+
+            // Add a delay for 1 second
+            Thread.sleep(1000);
+
+            // Click on the Log in button in the top right corner.
+            Selenide.$("#topnav > a.signin").click();
+
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
+
+            // Log in to the Kronox website
+            loginKronoxWebsite();
+
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
+
+            // Click on the login button element
+            Selenide.$x("//*[@id=\"login_button\"]").click();
+
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
+
+            // Click on the "Aktivitetsanmälan" menu button element
+            Selenide.$("body > div.main > div.header > div > div > ul > li:nth-child(9) > a > em > b").click();
+
+            // Add a delay for 2 seconds
+            Thread.sleep(2000);
+
+            // Check if the expected text is present on the page
+            String expectedText = "Datum: 2023-05-30";
+            String actualText = Selenide.$("body").getText();
+            assertTrue(actualText.contains(expectedText));
 
 
+            boolean testPassed = actualText.contains(expectedText);
+            reportTestResult(testPassed);
+
+
+            // Add a delay for 1 seconds
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // Handle InterruptedException
+            logger.error("InterruptedException occurred.", e);
+            throw e;
+        }
+    }
+
+    private void reportTestResult(boolean testPassed) {
+        if (testPassed) {
+            logger.info("Test passed successfully.");
+        } else {
+            logger.error("Test failed.");
+        }
     }
 
     private void loginKronoxWebsite() throws InterruptedException {
@@ -48,11 +95,13 @@ public class FinalExaminationInfoTest implements TestSetup {
 
         try {
             logger.info("Reading username and password from kronoxConfig.json");
+
             // Read the email and password from kronoxConfig.json
             Gson gson = new Gson();
             JsonElement config = gson.fromJson(new FileReader("kronoxConfig.json"), JsonElement.class);
             usernameKronox = config.getAsJsonObject().get("usernameKronox").getAsString();
             passwordKronox = config.getAsJsonObject().get("passwordKronox").getAsString();
+
             logger.info("Read successfully!");
         } catch (IOException e) {
             // Handle any exceptions that might occur while reading the file
@@ -60,18 +109,31 @@ public class FinalExaminationInfoTest implements TestSetup {
             System.exit(1);
         }
 
-        fillInKronoxUsernameAndPassword(usernameKronox, passwordKronox);
 
+
+        fillInKronoxUsernameAndPassword(usernameKronox, passwordKronox);
     }
 
     private void fillInKronoxUsernameAndPassword(String usernameKronox, String passwordKronox) throws InterruptedException {
+
+        try {
+        // Fill in the password field
         SelenideElement passID = $("#login_password");
         passID.setValue(passwordKronox);
         Thread.sleep(1000);
 
+        // Fill in the username field
         SelenideElement userID = $("#login_username");
         userID.setValue(usernameKronox);
         Thread.sleep(1000);
-
+    } catch (InterruptedException e) {
+            // Handle InterruptedException
+            logger.error("InterruptedException occurred while filling in Kronox username and password.", e);
+            throw e;
+        } catch (Exception e) {
+            // Handle any other exceptions that might occur
+            logger.error("An error occurred while filling in Kronox username and password.", e);
+            throw e;
+        }
     }
 }
--- src/test/java/TranscriptCreationTest.java
+++ src/test/java/TranscriptCreationTest.java
@@ -1,7 +1,79 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author Johan Lund
  * @project LTU-tests
  * @date 2023-05-16
  */
-public class TranscriptCreationTest {
+public class TranscriptCreationTest implements TestSetup {
+
+    private static final Logger logger = LoggerFactory.getLogger(TranscriptCreationTest.class);
+    //@Disabled
+    @Test
+    public void testTranscriptCreation() throws InterruptedException {
+        //try {
+        // Click on the menu "Intyg", which will direct us to Ladok.
+        Selenide.$x("/html/body/div/div[1]/div[4]/div[1]/div[2]/div[2]/ul[2]/li[4]/a").click();
+        Thread.sleep(2000);
+        // Switch to the new window for Ladok
+        Selenide.switchTo().window(1);
+        // Accept cookies
+        Selenide.$x("/html/body/ladok-root/ladok-cookie-banner/div/div/div/div/div/div[2]/button[1]").click();
+        Thread.sleep(2000);
+        // Click on the "Access through your institution" button on Ladok log in.
+        Selenide.$("body > ladok-root > div > main > div > ladok-inloggning > div > div > div > div > div > div > div > ladok-student > div:nth-child(2) > a").click();
+        Thread.sleep(2000);
+        // Clicks on the search bar, makes the search for Luleå University and thereby the log in is completed.
+        SelenideElement searchInput = $("#searchinput");
+        searchInput.setValue("Luleå university");
+        Selenide.$("#ds-search-list > a > li > div > div.text-truncate.label.primary").click();
+        Thread.sleep(2000);
+        // Click on the "Hamburger menu" in the top right corner.
+        Selenide.$("#sticky-header > ladok-top-meny > div > div > div:nth-child(2) > button > fa-icon > svg").click();
+        Thread.sleep(2000);
+        // Click on the menu "Transcripts and certificates".
+        Selenide.$("#sidomeny-ul > li:nth-child(3) > ladok-behorighetsstyrd-nav-link > a").click();
+        Thread.sleep(2000);
+        // Click on the button "Create" to begin the creation of a transcript.
+        Selenide.$("#main > div > ladok-intyg > ladok-skapa-intyg-knapp > div > button").click();
+        Thread.sleep(2000);
+        // Clicks on the dropdown menu and pick the correct option by indexing the alternatives.
+        SelenideElement dropDown = $("#intygstyp");
+        dropDown.selectOption(2);
+        Thread.sleep(2000);
+        Selenide.$("#main > div > ladok-skapa-intyg > ladok-card > div > div > ladok-card-body > div:nth-child(3) > div > form > div:nth-child(3) > div > ladok-skapa-intyg-knapprad > div > button.btn.btn-ladok-brand.text-nowrap.me-lg-3").click();
+        Thread.sleep(2000);
+
+        String expectedTranscriptStatus = "Official transcript of records";
+        String actualTranscriptStatus = Selenide.$("body").getText();
+        assertEquals(expectedTranscriptStatus, actualTranscriptStatus);
+
+        boolean testPassed = actualTranscriptStatus.contains(expectedTranscriptStatus);
+        reportTestResultTranscript(testPassed);
+
+
+
+
+
+
+
+        //}
+    }
+    private void reportTestResultTranscript(boolean testPassed) {
+        if (testPassed) {
+            logger.info("Test passed successfully.");
+        } else {
+            logger.error("Test failed.");
+        }
+    }
 }
